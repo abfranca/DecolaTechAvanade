8b707f19c52ae6dc24a03ed0d07b24ed0a5f2069
Alexandre B França <alexandre.b.franca@gmail.com>
2021-09-18 16:03:07 -0300

    Criando uma aplicação console.

---
+++ DigitalInnovationOne/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 10; i++){
+                Console.WriteLine($"Iteração nº {i+1}.");
+            }
+        }
+    }
+}
